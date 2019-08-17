b7a9e4736178477bb9d49a9703a28dcf45b46b9e
leanghorn <n.leanghorn@gmail.com>
2019-08-17 21:04:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function(req, res) {
-		res.send("Hello app1");
+		res.send("Hello Nodejs project");
 });
 
 app.listen(8080, function(){
