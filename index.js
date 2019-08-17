1d4f7eecb5dbe3a13b2661d6eacdc29a04c698ee
leanghorn <n.leanghorn@gmail.com>
2019-08-17 21:27:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ app.get('/', function(req, res) {
 		res.send("Hello Nodejs project");
 });
 
+app.get('/hi', function(req, res) {
+		res.send("Hello how are you");
+});
+
 app.listen(8080, function(){
 		console.log('app is nuning in port 8080');
 });
